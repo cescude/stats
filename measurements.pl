8c7c0467fa46d56d44f076a8594e3b2a675414b7
Chandler Escude <chandler.escude@gmail.com>
2020-04-21 07:50:59 -0400

    Adding log data generator

---
+++ measurements.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+sub rr {
+  my ($max) = @_;
+  int(rand($max))
+}
+
+sub m0 {
+  printf("%dms running, size: %d\n", rr(100000), rr(200));
+}
+sub m1 {
+  printf("found %d of %d users,\n  %dkb\n", rr(5), 5 + rr(20), rr(1024*1024));
+}
+sub m2 {
+  my @words = qw{one two three four five six seven eight nine ten red blue
+                 green yellow purple orange banana apple pineapple horse};
+  printf("Status count: %d of %s\n", rr(9999), $words[rr(scalar @words)]);
+}
+
+while (1) {
+  m0;
+  m1;
+  m2;
+}
